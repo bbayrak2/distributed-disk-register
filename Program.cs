b38526fe3d43837f09d9befeb43e0d2f697d869b
bbayrak2 <burak.bayrak@bil.omu.edu.tr>
2025-12-14 19:22:20 +0300

    test sunucusu

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+int port = 5000;
+TcpListener server = new TcpListener(IPAddress.Any, 5000);
+server.Start();
+
+Console.WriteLine("Sunucu çalışıyor...");
+
+while (true)
+{
+    TcpClient client = server.AcceptTcpClient();
+    Console.WriteLine("İstemci bağlandı.");
+
+    NetworkStream stream = client.GetStream();
+    byte[] buffer = new byte[1024];
+
+    while (true)
+    {
+        int count = stream.Read(buffer, 0, buffer.Length);
+        if (count == 0)
+        {
+            Console.WriteLine("İstemci bağlantıyı kapattı.");
+            break;
+        }
+
+        string msg = Encoding.UTF8.GetString(buffer, 0, count);
+        Console.WriteLine("Gelen: " + msg);
+
+        File.AppendAllText("messages.txt", msg + Environment.NewLine);
+    }
+
+    client.Close();
+}
