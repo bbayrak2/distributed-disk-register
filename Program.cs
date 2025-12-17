0ae11eab53678416324f581f7ad60801e5368fef
bbayrak2 <burak.bayrak@bil.omu.edu.tr>
2025-12-17 22:57:27 +0300

    Test server new vesion

--- Program.cs
+++ Program.cs
@@ -3,35 +3,194 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Microsoft.Data.Sqlite;
 
 int port = 5000;
-TcpListener server = new TcpListener(IPAddress.Any, 5000);
-server.Start();
+string dbPath = "Data Source=database1.db";
 
-Console.WriteLine("Sunucu çalışıyor...");
+PrepareDatabase(dbPath);
+
+TcpListener server = new TcpListener(IPAddress.Any, port);
+server.Start();
+Console.WriteLine($"Sunucu çalışıyor (Port: {port})...");
 
 while (true)
 {
     TcpClient client = server.AcceptTcpClient();
-    Console.WriteLine("İstemci bağlandı.");
 
-    NetworkStream stream = client.GetStream();
-    byte[] buffer = new byte[1024];
+    Task.Run(() => HandleClient(client, dbPath));
+}
 
-    while (true)
+void HandleClient(TcpClient client, string connectionString)
+{
+    using (client)
+    using (NetworkStream stream = client.GetStream())
+    using (var reader = new StreamReader(stream, Encoding.UTF8))
+    using (var writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true })
+    using (var writerGetSet = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true })
+    using (var writeUyarı = new StreamWriter(stream, Encoding.UTF8))
     {
-        int count = stream.Read(buffer, 0, buffer.Length);
-        if (count == 0)
+        Console.WriteLine("İstemci bağlandı.");
+        string read()
         {
-            Console.WriteLine("İstemci bağlantıyı kapattı.");
-            break;
+            writerGetSet.WriteLine("Yapmak İstediğiniz İşlem :(GET/SET)");
+            string? process = reader.ReadLine();
+            return process;
         }
+        string process = read();
 
-        string msg = Encoding.UTF8.GetString(buffer, 0, count);
-        Console.WriteLine("Gelen: " + msg);
+        processControl(process);
+        string processControl(string? process)
+        {
+            if (process != null)
+            {
+                switch (process)
+                {
+                    case "GET":
 
-        File.AppendAllText("messages.txt", msg + Environment.NewLine);
+                        writer.WriteLine("Mesaj ID :");
+                        string? messageId = reader.ReadLine();
+                        if (!string.IsNullOrEmpty(messageId))
+                        {
+                            GET(connectionString, messageId, writer);
+                        }
+                        return processControl(read());
+                    case "SET":
+
+                        SET(writer, reader);
+                        return "SET";
+                    default:
+                        writeUyarı.WriteLine("Lütfen geçerli bir işlem giriniz(GET/SET)");
+                        string? process2 = reader.ReadLine();
+                        return processControl(process2);
+
+                }
+            }
+            else
+            {
+
+                if (process == null)
+                {
+
+                    return "DISCONNECTED";
+                }
+
+
+                writeUyarı.WriteLine("Lütfen geçerli bir işlem giriniz(GET/SET)");
+                string? process3 = reader.ReadLine();
+                return processControl(process3);
+            }
+            void SET(StreamWriter writer, StreamReader reader)
+            {
+
+
+                while (true)
+                {
+                    writer.WriteLine("Mesaj ID :");
+
+                    string? messageId = reader.ReadLine();
+                    if (string.IsNullOrEmpty(messageId)) return;
+
+                    writer.WriteLine("Mesajınızı Giriniz : ");
+                    string? msg = reader.ReadLine();
+
+                    if (msg == null) break;
+
+                    Console.WriteLine($"[{messageId}]: {msg}");
+
+
+                    SaveToSql(connectionString, messageId, msg);
+
+                    writer.WriteLine("SERVER_OK: Mesaj veritabanına kaydedildi.");
+
+                    string process = read();
+
+                    processControl(process);
+
+                }
+            }
+
+        }
+        Console.WriteLine("İstemci ayrıldı.");
+
+
+
+
+
+
+    }
+}
+    
+
+    void PrepareDatabase(string connectionString)
+    {
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        connection.Open();
+        string tableCmd = @"CREATE TABLE IF NOT EXISTS Messages (
+            MessageId TEXT UNIQUE, 
+            Content TEXT
+        )";
+
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = tableCmd;
+            command.ExecuteNonQuery();
+        }
+    }
     }
 
-    client.Close();
+void SaveToSql(string connectionString, string messageId, string content)
+{
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        connection.Open();
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = "INSERT OR IGNORE INTO Messages (MessageId, Content) VALUES ($id, $content)";
+            command.Parameters.AddWithValue("$id", messageId);
+            command.Parameters.AddWithValue("$content", content);
+            command.ExecuteNonQuery();
+        }
+    }
+}
+
+
+
+void GET(string connectionString, string targetId , StreamWriter writer) 
+{
+    try
+    {
+        using (var connection = new SqliteConnection(connectionString))
+
+        {
+            connection.Open();
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT Content FROM Messages WHERE MessageId = $id";
+                command.Parameters.AddWithValue("$id", targetId);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                    
+                        string content = reader.GetString(0);
+
+                        Console.WriteLine($"\n[BAŞARILI] ID: {targetId} bulundu.");
+                        writer.WriteLine($"İçerik: {content}");
+                    }
+                    else
+                    {
+                        writer.WriteLine($"\n[UYARI] {targetId} ID'li bir mesaj bulunamadı.");
+                    }
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        writer.WriteLine("SQL Hatası: " + ex.Message);
+    }
+    
 }
--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b38526fe3d43837f09d9befeb43e0d2f697d869b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
