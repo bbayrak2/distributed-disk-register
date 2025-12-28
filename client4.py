1964a5f1b98b5b56ded1a90dd15bd10307655a15
bbayrak2 <burak.bayrak@bil.omu.edu.tr>
2025-12-28 19:23:08 +0300

    SET düzenlemesi

--- client4.py
+++ client4.py
@@ -6,18 +6,22 @@ class LeaderClient:
     def __init__(self, host="127.0.0.1", port=6666):
         self.host = host
         self.port = port
-        self.used_ids = set()  
+        self.path1 = # records path 
+        if self.path1:
+            ıd_tespit(self.path1) 
+        self.used_ids = set(ıdler) 
+        
     def send_command(self, command: str):
         command = command.strip()
 
-        
         if command.upper().startswith("SET "):
-            try: 
+            try:
                 parts = command.split(" ", 2)
                 message_id = int(parts[1])
 
                 if message_id in self.used_ids:
                     print(f" HATA: {message_id} ID daha önce kullanıldı. Sunucuya gönderilmedi.")
+                    print(f"{max_ıd(self.path1)}")
                     return
 
                 self.used_ids.add(message_id)
@@ -55,18 +59,65 @@ def interactive_mode(client: LeaderClient):
             break
         client.send_command(cmd)
 
+
 def load_test_set(client: LeaderClient):
-    start_id = int(input("Başlangıç message_id girin (örn: 1): "))
+    
     n = int(input("Kaç adet SET gönderilsin?: "))
-    delay = 0.001
 
-    current_id = start_id
+    if client.used_ids:
+        baslangic_id = max(client.used_ids) + 1
+    else:
+        baslangic_id = 1
+    
+    current_id = baslangic_id
 
-    for _ in range(n):
+    for i in range(1, n + 1):
         client.send_command(f"SET {current_id} mesaj_{current_id}")
-        current_id += 1
-
+        current_id+=1
     print(f"{n} adet SET gönderildi.")
+sırala=[]
+ıdler=[]
+
+def max_ıd(yol):
+    ıd_tespit(yol)
+    if sırala:
+        print(f"mevcut max ıd {max(sırala)}  \ndaha buyuk bir id giriniz")
+def ıd_tespit(yol):
+            from pathlib import Path
+            ana_yol = Path(yol)
+
+
+            if not ana_yol.exists():
+                print("Yol bulunamadı.")
+                return
+            for klasor in ana_yol.iterdir():
+                if klasor.is_dir():
+                    ıd_tespit(klasor.resolve())
+                else:
+                    if klasor.is_file():
+                            sıra =int(klasor.stem)
+                            sırala.append(sıra)
+                            ıdler.append(sıra)
+
+
+def mesaj_sayisi_pathlib(yol):
+    from pathlib import Path
+    ana_yol = Path(yol)
+    
+    if not ana_yol.exists():
+        print("Yol bulunamadı.")
+        return
+
+    print(f"--- Tarama Sonuçları ---\n")
+
+
+    for klasor in ana_yol.iterdir():
+        if klasor.is_dir():
+            
+            
+            txt_sayisi = sum(1 for dosya in klasor.iterdir() 
+                    if dosya.is_file() and dosya.suffix.lower() == '.txt')
+            print(f"{klasor.name:<10} : {txt_sayisi} mesaj ")
 
 
 if __name__ == "__main__":
